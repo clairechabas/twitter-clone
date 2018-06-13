3a4ccd1c45a493b6597a8f15877e7a59a4163374
Chabou <claire.chabas@gmail.com>
2018-06-13 16:15:25 +0200

    Setting up bcrypt to hash passwords and compare the user's hashed password with the one saved in db

--- twitterclone-backend/models/user.js
+++ twitterclone-backend/models/user.js
@@ -24,6 +24,34 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// SAVING DOCUMENTS IN DB WITH A HASHED PASSWORD
+// Pre-save Hook : we run this async function before any document in Mongoose is saved.
+// This async function waits for the password to hash, set the password to be the hashed password
+// and then moves to the next piece of middleware, which is saving that specific document.
+userSchema.pre("save", async function(next) {
+  try {
+    if(!this.isModified("password")) {
+      return next();
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch(err) {
+      return next(err);
+  }
+});
+
+// PASSWORD COMPARISON
+// Compares a hashed password with the user's hased password saved in db.
+userSchema.method.comparePassword = async function(candidatePassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch(err) {
+      return next(err);
+  }
+}
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
