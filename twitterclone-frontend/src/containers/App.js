d3b3a4645b09b6c49ace678e4e53e68b058b89e2
Chabou <claire.chabas@gmail.com>
2018-06-14 16:29:52 +0200

    Implementing the NavBar component and its styling

--- twitterclone-frontend/src/containers/App.js
+++ twitterclone-frontend/src/containers/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '../store';
 import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './NavBar';
 
 const store = configureStore()
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>Hello Leo !</div>
+      <div className="onboarding">
+        <NavBar />
+      </div>
     </Router>
   </Provider>
 );
