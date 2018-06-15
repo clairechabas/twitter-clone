5100b576f9ebf7440fa7f5823bf7f9ad44418fe7
Chabou <claire.chabas@gmail.com>
2018-06-15 12:43:32 +0200

    Implementing Log out

--- twitterclone-frontend/src/containers/NavBar.js
+++ twitterclone-frontend/src/containers/NavBar.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { logout } from '../store/actions/auth';
 import Logo from '../images/warbler-logo.png';
 
 class NavBar extends Component {
+  logout = e => {
+    e.preventDefault();
+      this.props.logout();
+  }
+
   render() {
     return(
       <nav className="navbar navbar-expand">
@@ -11,11 +17,17 @@ class NavBar extends Component {
           <div className="navbar-header">
           <Link to="/" className="navbar-brand"><img src={Logo} alt="TwitterClone Home" /></Link>
           </div>
-        
-          <ul className="nav navbar-nav navbar-right">
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/signin">Sign In</Link></li>
-          </ul>
+          {this.props.currentUser.isAuthenticated ? (
+            <ul>
+              <li><Link to={`/users/${this.props.currentUser.user.id}/messages/new`}>New Message</Link></li>
+              <li><a onClick={this.logout}>Log out</a></li>
+            </ul>
+          ) : (
+            <ul className="nav navbar-nav navbar-right">
+              <li><Link to="/signup">Sign Up</Link></li>
+              <li><Link to="/signin">Log In</Link></li>
+            </ul>
+          )}
         </div>
       </nav>
     );
@@ -28,4 +40,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, null)(NavBar);
+export default connect(mapStateToProps, {logout})(NavBar);
